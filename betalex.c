0247b3a7d0e3cb3bdfd45c5425a074641f7e67dc
Maxnuggets <jericholongabela@gmail.com>
2022-02-25 01:52:50 +0800

    updated betalex

--- betalex.c
+++ betalex.c
@@ -1,10 +1,42 @@
 #include "main.h"
 
-int main (){
-	char currchar = '^';
-	state currstate = q0;
+	bool isInTransition = false;
 
-	printf("my current state is q%d\n", currstate);
-	currstate = lookup_dest(currstate, currchar);
-	printf("my current state is q%d", currstate);
+void lexical();
+
+int main (){
+	char chararray [] = "++\0"; //file content
+	state currstate = 0;
+	char currchar;
+	int j = 0;
+	currchar = chararray [j];
+	currstate = transition (currstate, currchar);
+	currchar = chararray [j+1];
+	currstate = transition (currstate, currchar);
+
+	printf("this is my current state: %d", currstate);
 }
+
+
+void lexical (){
+	char chararray [] = "++\0";
+	state currstate = 0;
+	char currchar;
+	int j = 0;
+	while (currchar != '\0'){
+		currchar = chararray [j];
+		switch (currstate){
+			case q0: transition(currstate, currchar);
+					break;
+			case q1: 
+			case q2: transition (currstate, currchar);
+			case q8: 
+
+			default: {
+				printf("Lexical Error");
+				break;
+				}
+	}
+		printf("%d", currstate);
+	} // end of while
+} // end of lexical
--- main.h
+++ main.h
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int i = 0;
+int i=0;
+
 
 typedef enum states{
-	q0, q1, q2, q3, q4, q5, q6, q7
+	q0, q1, q2, q3, q4, q5, q6, q7, q8
 }state;
 
 struct transition {
@@ -14,21 +16,74 @@ struct transition {
 };
 
 struct transition transition_table [] = {
-	{q0, '+', q2},
-	{q0, '-', q3},
-	{q0, '*', q4},
+	{q0, '+', q2}, // 0
+	{q0, '-', q3}, // 1
+	{q0, '*', q4}, // ...
 	{q0, '/', q5},
 	{q0, '%', q6},
-	{q0, '^', q7}
+	{q0, '^', q7},
+	{q2, '+', q8},
 };
 
-int lookup_dest (state srcstate, char currchar){
-    if (transition_table[i].src_state == srcstate && transition_table[i].reqChar == currchar){
-        return transition_table[i].dest_state;
-    }
-    else{
-        i++;
-        lookup_dest (srcstate, currchar);
-    }
-    
+bool find_dest(state srcstate, char currchar){
+    if (srcstate == transition_table[i].src_state && currchar == transition_table[i].reqChar){
+		return true;
+	}
+	else if (i == 7){ // 7 refers to transition table size
+		return false;
+	}else
+		i++;
+		find_dest(srcstate, currchar);
+}
+
+int get_dest(){
+	int tobereturned = transition_table[i].dest_state;
+	i = 0;
+	return tobereturned;
+}
+
+void accept (){
+}
+
+void printcontents(char *c, int counter){
+    for (int i = 0; i < counter; i++) {
+      printf("%c", c[i]);
+      }
+}
+
+char openfile (char *pplfile){
+    int counter = 0;
+	char* chpt;
+    char ch;
+    FILE* fp;
+
+      fp = fopen(pplfile, "r");
+      if (fp == NULL) {
+    		printf("Can't open file\n");
+    	}
+
+      	while ((ch = fgetc(fp)) != EOF) {
+        counter++;
+    	}
+      
+		rewind(fp);
+
+    	chpt = malloc(counter * sizeof(char));
+
+    	fread(chpt, counter * sizeof(char), 1, fp);
+
+    	printcontents(chpt, counter);
+
+      fclose(fp);
+      free(chpt);
+}
+
+int transition (state currstate, char currchar){
+	if (find_dest(currstate, currchar) == true){
+		currstate = get_dest();
+		printf("Current state is: q%d\n", currstate);
+		return currstate;
+	}else{
+		return -1;
+	}
 }
