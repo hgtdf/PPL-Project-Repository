9c7ad73ce3f8f07cc7eac67e55dbe2f480b84e5a
Maxnuggets <jericholongabela@gmail.com>
2022-02-26 23:05:28 +0800

    updated betalex.c

--- betalex.c
+++ betalex.c
@@ -1,42 +1,118 @@
 #include "main.h"
 
-	bool isInTransition = false;
+bool isInTransition = false;
+state currstate = q0;
+int buffstate, buffcount = 0; // parang pareho lang tong currstate sa int charClass sa front.c
+int counter = 0;
+char currchar = 0;
+char nextchar = 0;
+char lexeme[50] = {'\0'};
+FILE *fileptr = NULL;
+char *filecontent = NULL; 
+int pos = 0;
 
+int checkfile();
+void openfile();
 void lexical();
+char getNonBlank();
+char getChar();
+void processfile();
+int transition(state, char);
+bool find_dest(state , char);
+int get_dest();
+void accept();
+void reset();
 
 int main (){
-	char chararray [] = "++\0"; //file content
-	state currstate = 0;
-	char currchar;
-	int j = 0;
-	currchar = chararray [j];
-	currstate = transition (currstate, currchar);
-	currchar = chararray [j+1];
-	currstate = transition (currstate, currchar);
-
-	printf("this is my current state: %d", currstate);
-}
-
+	processfile();
+	lexical();
+	free(filecontent);
+} // end of main
 
 void lexical (){
-	char chararray [] = "++\0";
-	state currstate = 0;
-	char currchar;
-	int j = 0;
-	while (currchar != '\0'){
-		currchar = chararray [j];
+	int i = 0;
+	currchar = getNonBlank();
+	printf("Pumasok ako kay lexical. With state %d currchar %c and lexeme of: %s\n", currstate, currchar, lexeme);
+	while (i < counter+1){
+		printf("Pumasok ako sa while. With state %d currchar %c and lexeme of: %s\n", currstate, currchar, lexeme);
 		switch (currstate){
-			case q0: transition(currstate, currchar);
+			case q0: 
+					printf("Pumasok ako sa case 0\n");
+					currstate = transition(currstate, currchar);
+					printf("naghanap ako ng transition. at nakuha ko si q%d\n", currstate);
+					lexeme[buffcount] = currchar;
+					printf("ipinasok ko si currchar: %c sa lexeme[%d] kaya ang laman na niya ay: -%s-\n", currchar, buffcount, lexeme);
+					buffcount++;
 					break;
 			case q1: 
-			case q2: transition (currstate, currchar);
-			case q8: 
+			case q2: currchar = getNonBlank();
+					printf("ngayon ang nextchar ko ay: %c, at ang state ko ay q%d\n", currchar, currstate);
+					if (find_dest(currstate, currchar) == true){
+						lexeme[buffcount] = currchar;
+						buffcount++;
+						currstate = get_dest();
+					}else {
+						printf("ang next char ay wala sa transition kaya back to state 0 na at accept na muna ako\n");
+						accept(lexeme, buffcount);
+						reset(&buffcount, &currstate);
+						lexeme[50] = 0;
+					}
+					break;
+			case q5: printf("Pumasok ako sa case 5\n");
+						accept(lexeme, buffcount);
+						reset(&buffcount, &currstate);
+						lexeme[50] = 0;
+						break;
+			case q8: printf("Pumasok ako sa case 8\n");
+						accept(lexeme, buffcount);
+						reset(&buffcount, &currstate);
+						lexeme[50] = 0;
+						break;
 
 			default: {
 				printf("Lexical Error");
+				reset(&buffcount, &currstate);
+				lexeme[50] = 0;
 				break;
 				}
-	}
-		printf("%d", currstate);
-	} // end of while
+		} // end of switch
+		i++;
+		printf("q%d when exiting switch\n", currstate);
+	}// end of while != '\0'
+		
 } // end of lexical
+
+void processfile(){
+	checkfile("sample.simp"); // check if filetype is accepted, else exit code immediately
+	fileptr = fopen("sample.simp", "r");
+	if (fileptr == NULL)
+		printf("Error opening file.");
+	
+	while (fgetc(fileptr) != EOF){
+		counter++;
+	}
+	rewind(fileptr); //reset ng fileptr
+	
+	//printf("filecounter[%d]\n", counter);
+	//copy contents of file to *filecontent
+	filecontent = malloc (counter * sizeof(char) + 1);
+	fread (filecontent, counter * sizeof(char), 1, fileptr);
+	filecontent[counter] = '\0';
+	//printf("filecontent = -%s-\n", filecontent);
+	fclose(fileptr);
+}
+
+char getNonBlank(){
+	while (isspace(filecontent[pos])){
+		printf("the content of filecontent[%d] is : %c\n", pos, filecontent[pos]);
+		pos++;
+	}
+	return getChar();
+} // end of getNonBlank
+
+char getChar(){
+	int p = pos;
+	printf("kinuha ko si filecontent[%d]\n", p);
+	pos++;
+	return filecontent[p];
+}
--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stddef.h>
 
 int i=0;
 
@@ -25,65 +28,73 @@ struct transition transition_table [] = {
 	{q2, '+', q8},
 };
 
-bool find_dest(state srcstate, char currchar){
-    if (srcstate == transition_table[i].src_state && currchar == transition_table[i].reqChar){
+bool find_dest(state srcstate, char currchar){//checks if specified transition is present in transitions_table[]
+    printf("ako si find dest, tinawag mo ako ang laman ng i ko ngayon ay %d hinahanap ko si srcstate: %d, at si currchar: %c\n", i, srcstate, currchar);
+	if (srcstate == transition_table[i].src_state && currchar == transition_table[i].reqChar){
 		return true;
 	}
-	else if (i == 7){ // 7 refers to transition table size
+	else if (i == 6){ // The number here, refers to transition_table[] size
+		i=0;
 		return false;
 	}else
 		i++;
 		find_dest(srcstate, currchar);
 }
 
-int get_dest(){
+void accept (char lexeme[], int buffcount){
+	int i;
+	for (i=0; i<buffcount; i++){
+		printf("%c", lexeme[i]);
+	}
+	printf("\n");
+}
+
+void reset (int *buffcount, state *currstate){
+	*currstate = q0;
+	*buffcount = 0;
+}
+
+int get_dest(){	//gets the dest_state of the specified transition
 	int tobereturned = transition_table[i].dest_state;
 	i = 0;
 	return tobereturned;
 }
 
-void accept (){
-}
-
-void printcontents(char *c, int counter){
-    for (int i = 0; i < counter; i++) {
-      printf("%c", c[i]);
-      }
-}
-
-char openfile (char *pplfile){
-    int counter = 0;
-	char* chpt;
-    char ch;
-    FILE* fp;
-
-      fp = fopen(pplfile, "r");
-      if (fp == NULL) {
-    		printf("Can't open file\n");
-    	}
-
-      	while ((ch = fgetc(fp)) != EOF) {
-        counter++;
-    	}
-      
-		rewind(fp);
-
-    	chpt = malloc(counter * sizeof(char));
-
-    	fread(chpt, counter * sizeof(char), 1, fp);
-
-    	printcontents(chpt, counter);
-
-      fclose(fp);
-      free(chpt);
-}
-
-int transition (state currstate, char currchar){
+int transition (state currstate, char currchar){ //function for transitioning states into another state
 	if (find_dest(currstate, currchar) == true){
 		currstate = get_dest();
-		printf("Current state is: q%d\n", currstate);
 		return currstate;
 	}else{
-		return -1;
+		return 1;
 	}
+}
+
+int checkfile (char *plfile){ // check for file's filetype
+    char extn[] = ".simp";
+    int len1 = strlen(plfile);
+    int len2 = strlen(extn);
+    int found = 0;
+    
+    if(len1 >= len2) {
+        for(int s1=0, s2=0; s1<len1; s1++) {
+            do {
+                if(plfile[s1] == extn[s2]) {
+                    if(++found == len2) return 1;
+                    s1++;
+                    s2++;
+                }
+                else {
+                s1 -= found; 
+                found = 0; 
+                s2 = 0;
+                }
+            } while(found);
+        }
+        printf("File extension not recognized.\n");
+        exit(1);
+    }
+    else {
+        printf("File invalid.");
+        exit(1);
+    }
 }
